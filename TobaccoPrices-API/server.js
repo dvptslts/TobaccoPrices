53db0fd7d538a02fccc6a44eef8f7d82ff6aac22
DVPT Solutions <dvptsolutions@gmail.com>
2022-04-25 16:34:30 +0200

    Basic backend, React in progress

--- TobaccoPrices-API/server.js
+++ TobaccoPrices-API/server.js
@@ -2,36 +2,66 @@ const express = require('express')
 const excelToJson = require('convert-excel-to-json')
 const fs = require('fs')
 const https = require('https')
+const mysql = require('mysql')
+const CronJob = require('cron').CronJob
 
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  //Download xlsx
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'dvptsolutions',
+  password: 'Dvptsolutions2022',
+  database: 'tobaccoprices'
+})
+
+var updateCronJob = new CronJob({
+  cronTime: '06 00 00 * * * ',
+  onTick: update(),
+  start: true,
+  runOnInit: false
+});
+
+function update() {
+  //DownloadXlsx
   let url = 'https://sztfh.hu/downloads/dohany/arkozzetetel/alkalmazando_dohanygyartmany_kiskereskedelmi_eladasi_arak_20220422120001.xlsx'
   https.get(url ,(res) => {
     res.pipe(fs.createWriteStream('tmp/source.xlsx'));
   })
 
+  //XlsxToJson
   const result = excelToJson({
     sourceFile: './tmp/source.xlsx',
     name: 'Munka1',
     range: 'A3:J30000',
     columnToKey: {
-      A: "Termékregisztrátor megnevezése",
-      B: "NDN cikkszám",
-      C: "EAN kód",
-      D: "Termék típus",
-      E: "Termék megnevezése",
-      F: "Márkanév",
-      G: "Mennyiség",
-      H: "Mennyiségi egység",
-      I: "Kiskereskedelmi eladási ár",
-      J: "Érvényesség kezdete"
+      A: "manufacturer",
+      B: "ndn",
+      C: "ean",
+      D: "category",
+      E: "name",
+      F: "brand",
+      G: "qtn",
+      H: "qtntype",
+      I: "price",
+      J: "date"
     }
   })
+  
+  for (item of result.Munka1) {
+    pool.query('REPLACE INTO data VALUES (\"'+item.manufacturer+'\", \"'+item.ndn+'\", \"'+item.ean+'\", \"'+item.category+'\", \"'+item.name+'\", \"'+item.brand+'\", '+item.qtn+', \"'+item.qtntype+'\", '+item.price+')', (err, rows, field) => {})
+  }
+}
 
-  res.json(result)
+app.get('/update', (req, res) => {
+  update()
+  res.end()
+})
+
+app.get('/priceList', (req, res) => {
+  pool.query('SELECT * FROM data', (err, rows, field) => {
+    res.send(rows)
+  })
 })
 
 app.listen(port, () => {
